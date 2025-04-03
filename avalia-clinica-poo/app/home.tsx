2fb24e8d5b307a262972ee65ebe9f226f8d33982
Thiago Akira <139590126+AkiraGitDev@users.noreply.github.com>
2025-04-03 16:37:03 -0300

    [ADD] Home Screen

---
+++ avalia-clinica-poo/app/home.tsx
@@ -0,0 +1,48 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>NomeApp</Text>
+      
+      <Link href="/cadastrarPaciente" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Realizar Cadastro</Text>
+        </TouchableOpacity>
+      </Link>
+
+      <Link href="/login" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: '#E6E6FA',
+    padding: 15,
+    borderRadius: 5,
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+});
